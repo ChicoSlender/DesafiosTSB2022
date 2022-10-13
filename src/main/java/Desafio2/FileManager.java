8ff7e7dccab4c181a0f9305f82cd8bb1d5ce1645
BenjaminGomez <benjamingomez@ravegan.com>
2022-10-12 21:34:49 -0300

    Added lote01.txt and lote02.txt to Desafio2 resource folder and added FileManager util class with method to extract int array from file

---
+++ src/main/java/Desafio2/FileManager.java
@@ -0,0 +1,39 @@
+package Desafio2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+//@TODO: Move class to a shared util module
+public class FileManager
+{
+    public Integer[] extractIntArrayFromFile(String fileName) {
+        ArrayList<Integer> numberArray = new ArrayList<>();
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        File numberFile;
+        try {
+            numberFile = new File(Objects.requireNonNull(classLoader.getResource("Desafio1/lote01.txt")).toURI());
+        }
+        catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        Scanner sc;
+        try {
+            sc = new Scanner(numberFile);
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (sc.hasNextInt()) {
+            Integer num = sc.nextInt();
+            numberArray.add(num);
+        }
+
+        return numberArray.toArray(new Integer[] {});
+    }
+}
--- src/main/java/Desafio2/Principal.java
+++ src/main/java/Desafio2/Principal.java
@@ -1,7 +1,12 @@
 package Desafio2;
 
+import java.util.Arrays;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Desafio 2");
+        FileManager fileManager = new FileManager();
+        Integer[] arr = fileManager.extractIntArrayFromFile("Desafio2/lote01.txt");
+        System.out.println(Arrays.toString(arr));
     }
 }
